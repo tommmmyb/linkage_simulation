62cff81a91e3443022cbb88f42e5f45f44cf6e9f
Thomas <tabrown2000@gmail.com>
2023-11-04 02:08:30 -0500

    read from motiongen pdf

--- klann_simulation.py
+++ klann_simulation.py
@@ -7,30 +7,56 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 from linkages import Klann
+try:
+    from motiongen.read_motiongen import read_klann_specs
+    mg = True
+except ModuleNotFoundError as e:
+    print(e)
+    # use the following geometry if tabula library not found to read from motiongen file
+    geometry = dict(
+        L = [2.078, 1.016, 1.906, 1.196, 1.886, 1.789, 3.15, 2.251, 5.686],  # L's 0-8
+        phi = np.deg2rad(32.5),                       # fixed acute angle between L2 & L5
+        theta0 = np.pi,                               # fixed angle between grounds of 4-bar
+        theta_constraint = np.deg2rad(-58),           # fixed angle between grounds of 5-bar
+    )
+    mg = False
+
+
+params = dict(
+
+    ########################### common input parameters
+    cycles = 4,
+    input_angular_velocity = 1,
+    ##########################
+
+) | (geometry if not mg else dict())
+
 
 def kinematic_sim():
     kwargs = dict(
-        cycles = 4,
+
+        ############################### input parameters
         grab = 0.0,
         release = 0.38*2*np.pi,
-        L = [2.078, 1.016, 1.906, 1.196, 1.886, 1.789, 3.15, 2.251, 5.686],  # L's 0-8
-        phi = np.deg2rad(32.5),             # fixed acute angle between L2 & L5
         input_angle = np.deg2rad(210),
-        input_angular_velocity = 1,
-        theta0 = np.pi,                     # fixed angle between grounds of 4-bar
-        theta_constraint = np.deg2rad(-58),           # fixed angle between grounds of 5-bar
-        paths = [9,8]
-    )
+        paths = [-1,8]
+        ###############################
+
+    ) | params | (read_klann_specs('motiongen/klann_mechanism.pdf') if mg else dict())  # pull geometry parameters from motiongen pdf
     sys = Klann(**kwargs)
     sys.movement_sim(8)    # execute standard single-linkage-system movement relative to hook (link8)
+
+    # TODO: implement link velocity in LinkageSystem and get rid of all 'hook' functions
+    sys.hook_pos()
+    sys.hook_vel()
+    sys.arm_pos()
     
     fig, ax = plt.subplot_mosaic('AB;CC', constrained_layout=False, figsize=(15, 10))
     ax['B'].set_prop_cycle('color', plt.cm.viridis(np.linspace(0,1,10)))
-    ax['A'].plot(*sys.hook_pos(), label='hook path'); ax['A'].set_aspect('equal', adjustable='box')
-    arm, = ax['A'].plot(*sys.arm_pos, 'ro-')
-    xvel, yvel = sys.hook_vel()
-    ax['C'].plot(np.rad2deg(sys.theta[:, 1]), xvel, label='hook x-velocity')
-    ax['C'].plot(np.rad2deg(sys.theta[:, 1]), yvel, label='hook y-velocity')
+    ax['A'].plot(sys.hook_x, sys.hook_y, label='hook path'); ax['A'].set_aspect('equal', adjustable='box')
+    arm, = ax['A'].plot(sys.arm_x[:,0], sys.arm_y[:,0], 'ro-')
+    ax['C'].plot(np.rad2deg(sys.theta[:, 1]), sys.hook_xvel, label='hook x-velocity')
+    ax['C'].plot(np.rad2deg(sys.theta[:, 1]), sys.hook_yvel, label='hook y-velocity')
     vline = ax['C'].axvline(x=np.rad2deg(sys.start_angle), color='limegreen')
     ax['B'].set_xlim(np.min(sys.links[:,:,0]), np.max(sys.links[:,:,0])+0.25), ax['B'].set_ylim(np.min(sys.links[:,:,1]), np.max(sys.links[:,:,1])+0.25)
     ax['B'].set_aspect('equal', adjustable=None)
@@ -40,12 +66,10 @@ def kinematic_sim():
          link_points, = ax['B'].plot(sys.links[0, map, 0], sys.links[0, map, 1], 'o-')
          points.append(link_points)
 
-
     paths = []
     for i in sys.paths:
         path, = ax['B'].plot(*np.split(sys.links[:0, i, :], 2, axis=-1))
         paths.append(path)
-         
 
     axslider = fig.add_axes([0.2, 0.9, 0.65, 0.02])
     slider = Slider(
@@ -54,7 +78,6 @@ def kinematic_sim():
         valmin = 0,
         valmax = sys.num - 1,
         valinit = 0,
-        # valstep = 1
     )
 
     def update(val):
@@ -66,8 +89,8 @@ def kinematic_sim():
         for link, map in sys.connection_mapping.items():
             points[link].set_data(sys.links[int(slider.val), map, 0], sys.links[int(slider.val), map, 1])
 
-
     slider.on_changed(update)
+    fig.canvas.mpl_connect('key_press_event', lambda e: on_press(e, slider))
 
     # graph formatting
     for a in ax.values():
@@ -75,34 +98,16 @@ def kinematic_sim():
     plt.show()
 
 
-# add keyboard interaction to slider
-def on_press(event, slider):
-    if event.key == 'right' and slider.val < slider.valmax:
-        slider.set_val(slider.val + 3)
-    if event.key == 'left' and slider.val > slider.valmin:
-        slider.set_val(slider.val - 3)
-    if event.key == 'shift+right' and slider.val < slider.valmax:
-        slider.set_val(slider.val + 1)
-    if event.key == 'shift+left' and slider.val > slider.valmin:
-        slider.set_val(slider.val - 1)
-    if event.key == 'ctrl+right' and slider.val < slider.valmax:
-        slider.set_val(slider.val + 10)
-    if event.key == 'ctrl+left' and slider.val > slider.valmin:
-        slider.set_val(slider.val - 10)
-
-
 def two_phase():
     kwargs = dict(
-        cycles = 4,
-        L = [2.078, 1.016, 1.906, 1.196, 1.886, 1.789, 3.15, 2.251, 5.686],  # L's 0-8
-        phi = np.deg2rad(32.5),             # fixed acute angle between L2 & L5
-        input_angular_velocity = 1,
-        theta0 = np.pi,                     # fixed angle between grounds of 4-bar
-        theta_constraint = np.deg2rad(-58),           # fixed angle between grounds of 5-bar
-        paths = [9,8],
+
+        ########################## input parameters
+        paths = [-1,8],
         grab = np.pi + 0.35,
         release = 2*np.pi + 0.35
-    )
+        ##########################
+
+    ) | params| (read_klann_specs('motiongen/klann_mechanism.pdf') if mg else dict())
     # initialize linkage systems
     sys1 = Klann(input_angle=0, **kwargs)
     sys2 = Klann(input_angle=0, phase_shift=np.pi, origin=(0,0), **kwargs)
@@ -158,16 +163,14 @@ def two_phase():
 
 def three_phase():
     kwargs = dict(
-        cycles = 4,
-        L = [2.078, 1.016, 1.906, 1.196, 1.886, 1.789, 3.15, 2.251, 5.686],  # L's 0-8
-        phi = np.deg2rad(32.5),             # fixed acute angle between L2 & L5
-        input_angular_velocity = 1,
-        theta0 = np.pi,                     # fixed angle between grounds of 4-bar
-        theta_constraint = np.deg2rad(-58),           # fixed angle between grounds of 5-bar
-        paths = [9,8],
+
+        ######################### input parameters
+        paths = [-1,8],
         grab = 4/3*np.pi - 0.15,
         release = 2*np.pi - 0.15
-    )
+        #########################
+
+    ) | params | (read_klann_specs('motiongen/klann_mechanism.pdf') if mg else dict()) 
     # initialize linkage systems
     sys1 = Klann(input_angle=0, **kwargs)
     sys2 = Klann(input_angle=0, phase_shift=2/3*np.pi, origin=(0,0), **kwargs)
@@ -227,6 +230,22 @@ def three_phase():
     fig.canvas.mpl_connect('key_press_event', lambda e: on_press(e, slider))
 
     plt.show()
+
+
+# add keyboard interaction to slider
+def on_press(event, slider):
+    if event.key == 'right' and slider.val < slider.valmax:
+        slider.set_val(slider.val + 3)
+    if event.key == 'left' and slider.val > slider.valmin:
+        slider.set_val(slider.val - 3)
+    if event.key == 'shift+right' and slider.val < slider.valmax:
+        slider.set_val(slider.val + 1)
+    if event.key == 'shift+left' and slider.val > slider.valmin:
+        slider.set_val(slider.val - 1)
+    if event.key == 'ctrl+right' and slider.val < slider.valmax:
+        slider.set_val(slider.val + 10)
+    if event.key == 'ctrl+left' and slider.val > slider.valmin:
+        slider.set_val(slider.val - 10)
 
 
 if __name__ == '__main__':
---
+++ motiongen/read_motiongen.py
@@ -0,0 +1,26 @@
+from tabula import read_pdf
+from numpy import pi
+
+def read_klann_specs(file):
+    df = read_pdf(file, stream=True, pages=3)[0]
+    df.set_index('Linked Joints', inplace=True)
+    
+    links = [
+        'J1 - J2',        # L0
+        'J1 - J4',        # L1
+        'J4 - J5',        # L2
+        'J5 - J2',        # L3
+        'J2 - J3',        # L4
+        'J7 - J5',        # L5
+        'J6 - J3',        # L6
+        'J7 - J6',        # L7
+        'J6 - J8'         # L8
+    ]
+
+    L = df['Length [cm]'][links].to_numpy()*10
+    
+    theta0 = df['Angle [rad]']['J1 - J2'].astype(float)
+    theta_constraint = df['Angle [rad]']['J2 - J3'].astype(float)
+    phi = ( df['Angle [rad]']['J7 - J5'].astype(float) - df['Angle [rad]']['J4 - J5'].astype(float) - pi ) * -1
+
+    return dict(L = L, theta0 = theta0, theta_constraint = theta_constraint, phi = phi)
